b51c2e307fe9293fec26b1356ddfcc0e89496372
negtak <negtak@negtaknoMacBook-Pro.local>
2025-08-05 20:35:09 +0900

    feat(app2): fix statup message

--- app2/main.go
+++ app2/main.go
@@ -8,10 +8,10 @@ import (
 
 func main() {
 	fmt.Println("App2 Starting...")
-	
-	msg := shared.FormatMessage("Hello from App2!")
+
+	msg := shared.FormatMessage("Hello from App2!!")
 	fmt.Println(msg)
-	
+
 	result := shared.Multiply(5, 8)
 	fmt.Printf("5 × 8 = %d\n", result)
 }
