64fb1e46f97824105a4fcadfed3860305dbefa89
negtak <negtak@negtaknoMacBook-Pro.local>
2025-08-06 11:30:48 +0900

    Fix math logic

--- app1/main.go
+++ app1/main.go
@@ -12,6 +12,6 @@ func main() {
 	msg := shared.FormatMessage("Hello from App1!!")
 	fmt.Println(msg)
 
-	result := shared.Add(10, 20)
-	fmt.Printf("10 + 20 = %d\n", result)
+	result := shared.Add(20, 20)
+	fmt.Printf("20 + 20 = %d\n", result)
 }
