c759e060edb2d3875cd4206473277186bc87ec35
negtak <negtak@negtaknoMacBook-Pro.local>
2025-08-06 11:27:13 +0900

    feat(app1): fix startup message

--- app1/main.go
+++ app1/main.go
@@ -8,10 +8,10 @@ import (
 
 func main() {
 	fmt.Println("App1 Starting...")
-	
-	msg := shared.FormatMessage("Hello from App1!")
+
+	msg := shared.FormatMessage("Hello from App1!!")
 	fmt.Println(msg)
-	
+
 	result := shared.Add(10, 20)
 	fmt.Printf("10 + 20 = %d\n", result)
 }
